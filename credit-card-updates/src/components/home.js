f17da4c8be35cd260cf60e0f0f40d7cb4caf33a6
Aleksandra <tajna@gmail.com>
2020-05-13 10:49:45 +0200

    add image for home page

--- credit-card-updates/src/components/home.js
+++ credit-card-updates/src/components/home.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import creditCards from '../images/creditCards.jpg'
+import { ImgContainer } from '../styles/homeStyle'
 
 const Home = () => {
-    return <h1>Home.</h1>;
+    return (
+        <React.Fragment>
+            <h1>Home.</h1>
+            <ImgContainer>
+                <img src={creditCards} alt="credit cards"/>
+            </ImgContainer>
+        </React.Fragment>
+    );
 }
  
 export default Home;
---
+++ credit-card-updates/src/styles/homeStyle.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const ImgContainer = styled.div`
+    margin-left:146px;
+`
